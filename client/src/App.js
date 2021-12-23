ca2a8bacf7952f11bb8191884cdf164bb7ae5ea3
Deekshant Yadav <d33kshant@gmail.com>
2021-12-23 19:59:39 +0530

    Added search and cart routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,48 @@ import {
 	Route
 } from 'react-router-dom'
 import AuthProvider from './contexts/AuthContext'
+import CartProvider from './contexts/CartContext'
+import CartPage from './pages/CartPage'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
+import SearchPage from './pages/SearchPage'
 
 const App = () => {
 
 	const [user, setUser] = useState(null)
+	const [cart, setCart] = useState([])
 
 	useEffect(() => {
-		const data = JSON.parse(localStorage.getItem('user'))
-		if(data) {
-			setUser(data)
+		const _user = JSON.parse(localStorage.getItem('user'))
+		const _cart = JSON.parse(localStorage.getItem('cart'))
+		if(_user) {
+			setUser(_user)
+		}
+		if(Array.isArray(_cart)) {
+			setCart(_cart)
 		}
 	}, [])
 
+	const addItem = item => {
+		console.log(item)
+	}
+
+	const removeItem = id => {
+		console.log(id)
+	}
+
 	return (
 		<AuthProvider value={{ user, setUser }} >
-			<Router>
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/login" element={<LoginPage />} />
-				</Routes>
-			</Router>
+			<CartProvider value={{cart, addItem, removeItem}} >
+				<Router>
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/cart" element={<CartPage />} />
+						<Route path="/login" element={<LoginPage />} />
+						<Route path="/search" element={<SearchPage />} />
+					</Routes>
+				</Router>
+			</CartProvider>
 		</AuthProvider>
 	)
 }
