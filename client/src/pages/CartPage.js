f772e47df497f182aa5768bdf2751f0d9c2ecdce
Deekshant Yadav <d33kshant@gmail.com>
2021-12-23 19:57:49 +0530

    Added Cart Page

---
+++ client/src/pages/CartPage.js
@@ -0,0 +1,16 @@
+import AppBar from "../components/AppBar"
+import '../styles/CartPage.css'
+
+const CartPage = () => {
+	return (
+		<>
+			<AppBar />
+			<div className=" cart-page-main">
+				<div className="cart-page-container">
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default CartPage
