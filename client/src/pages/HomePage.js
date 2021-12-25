1b5522a88da3d11071b7c83717cc17cf9410b6b2
Deekshant Yadav <d33kshant@gmail.com>
2021-12-25 21:58:03 +0530

    Updated to load products dynamically

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Navigate } from "react-router-dom"
 import AppBar from "../components/AppBar"
 import Carousel from "../components/Carousel"
@@ -6,96 +6,23 @@ import Categories from "../components/Categories"
 import ProductCard from "../components/ProductCard"
 import ProductList from "../components/ProductList"
 import { AuthContext } from "../contexts/AuthContext"
+import axios from 'axios'
 
 const HomePage = () => {
 
 	const { user } = useContext(AuthContext)
+	const [products, setProducts] = useState([])
+
+	useEffect(() => {
+		axios.get('/api/v1/products').then(response => setProducts(response.data))
+		return () => {
+			setProducts([])
+		}
+	}, [])
 
 	if (!user) {
 		return <Navigate to="/login" />
 	}
-
-	const products = [
-		{
-			thumbs: [
-				"https://i.imgur.com/Z6Q0NNl.jpg",
-				"https://i.imgur.com/lFSZbCF.jpg"
-			],
-			options: ["7", "8", "9", "10", "11"],
-			tags: ["shoes", "fashion", "puma"],
-			name: "Unisex Navy Blue Wired Cage Sneakers",
-			category: "Fashion",
-			brand: "Puma",
-			price: 3499,
-			offer: 45,
-			description: "PRODUCT STORY\nWith its wavy IMEVA outsole and breathable air mesh upper, the PUMA Wired Cage shoes are just the right mix of performance and style. The elastic gore across the forefoot and bold PUMA branding ensure a stylish and snug fit, and the SoftFoam+ sockliner provides superior cushioning\n\nFEATURES + BENEFITS\nIMEVA: PUMA's midsole for a lightweight and comfortable feel\nSoftFoam+: PUMA's comfort sockliner for instant step-in and long-lasting comfort that provides soft cushioning every step of your day\nDETAILS\nLow boot\nMesh and synthetic leather upper\nRubber outsole\nPUMA Wordmark across toe\nPUMA Cat Logo on tongue tape\nCushioned footbed\nTextured and patterned outsole\nWarranty: 3 months\nWarranty provided by brand/manufacturer\n\nSize & Fit\nRegular\n\nMaterial & Care\nMesh\nWipe with a clean, dry cloth to remove dust"
-		},{
-			thumbs: [
-				"https://i.imgur.com/Z6Q0NNl.jpg",
-				"https://i.imgur.com/lFSZbCF.jpg"
-			],
-			options: ["7", "8", "9", "10", "11"],
-			tags: ["shoes", "fashion", "puma"],
-			name: "Unisex Navy Blue Wired Cage Sneakers",
-			category: "Fashion",
-			brand: "Puma",
-			price: 3499,
-			offer: 45,
-			description: "PRODUCT STORY\nWith its wavy IMEVA outsole and breathable air mesh upper, the PUMA Wired Cage shoes are just the right mix of performance and style. The elastic gore across the forefoot and bold PUMA branding ensure a stylish and snug fit, and the SoftFoam+ sockliner provides superior cushioning\n\nFEATURES + BENEFITS\nIMEVA: PUMA's midsole for a lightweight and comfortable feel\nSoftFoam+: PUMA's comfort sockliner for instant step-in and long-lasting comfort that provides soft cushioning every step of your day\nDETAILS\nLow boot\nMesh and synthetic leather upper\nRubber outsole\nPUMA Wordmark across toe\nPUMA Cat Logo on tongue tape\nCushioned footbed\nTextured and patterned outsole\nWarranty: 3 months\nWarranty provided by brand/manufacturer\n\nSize & Fit\nRegular\n\nMaterial & Care\nMesh\nWipe with a clean, dry cloth to remove dust"
-		},{
-			thumbs: [
-				"https://i.imgur.com/Z6Q0NNl.jpg",
-				"https://i.imgur.com/lFSZbCF.jpg"
-			],
-			options: ["7", "8", "9", "10", "11"],
-			tags: ["shoes", "fashion", "puma"],
-			name: "Unisex Navy Blue Wired Cage Sneakers",
-			category: "Fashion",
-			brand: "Puma",
-			price: 3499,
-			offer: 45,
-			description: "PRODUCT STORY\nWith its wavy IMEVA outsole and breathable air mesh upper, the PUMA Wired Cage shoes are just the right mix of performance and style. The elastic gore across the forefoot and bold PUMA branding ensure a stylish and snug fit, and the SoftFoam+ sockliner provides superior cushioning\n\nFEATURES + BENEFITS\nIMEVA: PUMA's midsole for a lightweight and comfortable feel\nSoftFoam+: PUMA's comfort sockliner for instant step-in and long-lasting comfort that provides soft cushioning every step of your day\nDETAILS\nLow boot\nMesh and synthetic leather upper\nRubber outsole\nPUMA Wordmark across toe\nPUMA Cat Logo on tongue tape\nCushioned footbed\nTextured and patterned outsole\nWarranty: 3 months\nWarranty provided by brand/manufacturer\n\nSize & Fit\nRegular\n\nMaterial & Care\nMesh\nWipe with a clean, dry cloth to remove dust"
-		},{
-			thumbs: [
-				"https://i.imgur.com/Z6Q0NNl.jpg",
-				"https://i.imgur.com/lFSZbCF.jpg"
-			],
-			options: ["7", "8", "9", "10", "11"],
-			tags: ["shoes", "fashion", "puma"],
-			name: "Unisex Navy Blue Wired Cage Sneakers",
-			category: "Fashion",
-			brand: "Puma",
-			price: 3499,
-			offer: 45,
-			description: "PRODUCT STORY\nWith its wavy IMEVA outsole and breathable air mesh upper, the PUMA Wired Cage shoes are just the right mix of performance and style. The elastic gore across the forefoot and bold PUMA branding ensure a stylish and snug fit, and the SoftFoam+ sockliner provides superior cushioning\n\nFEATURES + BENEFITS\nIMEVA: PUMA's midsole for a lightweight and comfortable feel\nSoftFoam+: PUMA's comfort sockliner for instant step-in and long-lasting comfort that provides soft cushioning every step of your day\nDETAILS\nLow boot\nMesh and synthetic leather upper\nRubber outsole\nPUMA Wordmark across toe\nPUMA Cat Logo on tongue tape\nCushioned footbed\nTextured and patterned outsole\nWarranty: 3 months\nWarranty provided by brand/manufacturer\n\nSize & Fit\nRegular\n\nMaterial & Care\nMesh\nWipe with a clean, dry cloth to remove dust"
-		},{
-			thumbs: [
-				"https://i.imgur.com/Z6Q0NNl.jpg",
-				"https://i.imgur.com/lFSZbCF.jpg"
-			],
-			options: ["7", "8", "9", "10", "11"],
-			tags: ["shoes", "fashion", "puma"],
-			name: "Unisex Navy Blue Wired Cage Sneakers",
-			category: "Fashion",
-			brand: "Puma",
-			price: 3499,
-			offer: 45,
-			description: "PRODUCT STORY\nWith its wavy IMEVA outsole and breathable air mesh upper, the PUMA Wired Cage shoes are just the right mix of performance and style. The elastic gore across the forefoot and bold PUMA branding ensure a stylish and snug fit, and the SoftFoam+ sockliner provides superior cushioning\n\nFEATURES + BENEFITS\nIMEVA: PUMA's midsole for a lightweight and comfortable feel\nSoftFoam+: PUMA's comfort sockliner for instant step-in and long-lasting comfort that provides soft cushioning every step of your day\nDETAILS\nLow boot\nMesh and synthetic leather upper\nRubber outsole\nPUMA Wordmark across toe\nPUMA Cat Logo on tongue tape\nCushioned footbed\nTextured and patterned outsole\nWarranty: 3 months\nWarranty provided by brand/manufacturer\n\nSize & Fit\nRegular\n\nMaterial & Care\nMesh\nWipe with a clean, dry cloth to remove dust"
-		},{
-			thumbs: [
-				"https://i.imgur.com/Z6Q0NNl.jpg",
-				"https://i.imgur.com/lFSZbCF.jpg"
-			],
-			options: ["7", "8", "9", "10", "11"],
-			tags: ["shoes", "fashion", "puma"],
-			name: "Unisex Navy Blue Wired Cage Sneakers",
-			category: "Fashion",
-			brand: "Puma",
-			price: 3499,
-			offer: 45,
-			description: "PRODUCT STORY\nWith its wavy IMEVA outsole and breathable air mesh upper, the PUMA Wired Cage shoes are just the right mix of performance and style. The elastic gore across the forefoot and bold PUMA branding ensure a stylish and snug fit, and the SoftFoam+ sockliner provides superior cushioning\n\nFEATURES + BENEFITS\nIMEVA: PUMA's midsole for a lightweight and comfortable feel\nSoftFoam+: PUMA's comfort sockliner for instant step-in and long-lasting comfort that provides soft cushioning every step of your day\nDETAILS\nLow boot\nMesh and synthetic leather upper\nRubber outsole\nPUMA Wordmark across toe\nPUMA Cat Logo on tongue tape\nCushioned footbed\nTextured and patterned outsole\nWarranty: 3 months\nWarranty provided by brand/manufacturer\n\nSize & Fit\nRegular\n\nMaterial & Care\nMesh\nWipe with a clean, dry cloth to remove dust"
-		}
-	]
 
 	return (
 		<>
