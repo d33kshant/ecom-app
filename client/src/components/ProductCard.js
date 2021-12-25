d379e66dd3b6f50ad01ef5ca0d7f055b046dc2f0
Deekshant Yadav <d33kshant@gmail.com>
2021-12-25 21:57:17 +0530

    Updated to show offer if there is one

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -5,11 +5,12 @@ const ProductCard = ({ name, brand, description, thumbs, price, offer }) => {
 		<a className="product-card-container" href="/product" >
 			<img className="product-card-thumb" src={thumbs[0]} />
 			<div className="product-card-info" >
-				<span className="product-card-price-info" >
-					<span className="product-card-mrp" >{price - ((offer*price)/100)}₹</span>
-					<span className="product-card-offer" >• {offer}% off •</span>
+				<p className="product-card-mrp" >{price - ((offer*price)/100)}₹</p>
+				{ (offer > 0) && <span className="product-card-price-info" >
+					<span className="product-card-offer" >  {offer}% off  </span>
+					•
 					<span className="product-card-price" >{price}₹</span>
-				</span>
+				</span>}
 				<h3 className="product-card-title" >{name}</h3>
 				<p className="product-card-description" >{brand}</p>
 			</div>
