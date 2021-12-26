b29fd6feb4c58ca658fc1df4664e5ef6ed1f10f9
Deekshant Yadav <d33kshant@gmail.com>
2021-12-26 22:25:24 +0530

    Updated product href

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,9 +1,9 @@
 import "../styles/ProductCard.css"
 
-const ProductCard = ({ name, brand, description, thumbs, price, offer }) => {
+const ProductCard = ({ name, brand, _id, thumbs, price, offer }) => {
 	return (
-		<a className="product-card-container" href="/product" >
-			<img className="product-card-thumb" src={thumbs[0]} />
+		<a className="product-card-container" href={`/product/${_id}`} >
+			<img alt={name} className="product-card-thumb" src={thumbs[0]} />
 			<div className="product-card-info" >
 				<p className="product-card-mrp" >{price - ((offer*price)/100)}₹</p>
 				{ (offer > 0) && <span className="product-card-price-info" >
