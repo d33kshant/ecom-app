7c9fe30c40843fb3d9c22a82d2516cbe39ab5692
Deekshant Yadav <d33kshant@gmail.com>
2021-12-27 19:13:09 +0530

    Updated Cart Context

--- client/src/contexts/CartContext.js
+++ client/src/contexts/CartContext.js
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 
-export const CartContext = createContext({ cart: [], addItem: (item)=>{}, removeItem: (id)=>{} })
+export const CartContext = createContext({ cart: [], addItem: item=>{}, removeItem: id=>{}, deleteItem: id=>{}, clearCart: ()=>{} })
 
 const CartProvider = ({children, ...props}) => {
 	return (
