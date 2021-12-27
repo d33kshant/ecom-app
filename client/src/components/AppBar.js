5dc10b3c5886ab79f865d426fb82d03d0e3526b4
Deekshant Yadav <d33kshant@gmail.com>
2021-12-27 19:14:33 +0530

    Updated cart badge

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -1,11 +1,13 @@
 import { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
+import { CartContext } from '../contexts/CartContext'
 import '../styles/AppBar.css'
 
-const AppBar = ({count}) => {
+const AppBar = () => {
 
 	const [search, setSearch] = useState('')
 	const { user, setUser } = useContext(AuthContext)
+	const { cart } = useContext(CartContext)
 
 	const onInputChange = (event) => setSearch(event.target.value)
 
@@ -46,7 +48,7 @@ const AppBar = ({count}) => {
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 16 16">
 							<path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
 						</svg>
-						{ count && <div className="appbar-cart-badge">{count}</div>}
+						{ cart.length > 0 && <div className="appbar-cart-badge">{cart.length}</div>}
 					</a>
 					{ user && <a className="appbar-action" onClick={onSignOutClicked} title="Sign Out" href="/" >
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 16 16">
