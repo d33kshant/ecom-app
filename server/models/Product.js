513f74833f622dbbccfdf16f3e208b8a124a0585
Deekshant Yadav <d33kshant@gmail.com>
2021-12-28 23:35:24 +0530

    Updated Product Model

--- server/models/Product.js
+++ server/models/Product.js
@@ -8,7 +8,7 @@ const schema = new mongoose.Schema({
 	options: [String],
 	thumbs: [String], 
 	price: Number,
-	offer: Number
+	rate: Number
 })
 
 module.exports = mongoose.model('Product', schema)
