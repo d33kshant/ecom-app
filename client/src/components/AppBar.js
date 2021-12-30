230825e8c50555c10b41f39bd9af377ba2e8fd60
Deekshant Yadav <d33kshant@gmail.com>
2021-12-30 22:47:23 +0530

    Updated Badge Count

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -6,6 +6,7 @@ import '../styles/AppBar.css'
 const AppBar = () => {
 
 	const [search, setSearch] = useState('')
+	const [count, setCount] = useState(0)
 	const { user, setUser } = useContext(AuthContext)
 	const { cart } = useContext(CartContext)
 
@@ -27,7 +28,12 @@ const AppBar = () => {
 		if(query) {
 			setSearch(query)
 		}
-	}, [])
+		let _count = 0
+		for (let i = 0; i <cart.length; i++){
+			_count += cart[i].count
+		}
+		setCount(_count)
+	}, [cart])
 
 	return (
 		<div className="appbar-main" >
@@ -48,7 +54,7 @@ const AppBar = () => {
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 16 16">
 							<path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
 						</svg>
-						{ cart.length > 0 && <div className="appbar-cart-badge">{cart.length}</div>}
+						{ count > 0 && <div className="appbar-cart-badge">{count}</div>}
 					</a>
 					{ user && <a className="appbar-action" onClick={onSignOutClicked} title="Sign Out" href="/" >
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 16 16">
