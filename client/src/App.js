f1fd0d11e75638fadfaeb06c3f667302a386c0e3
Deekshant Yadav <d33kshant@gmail.com>
2021-12-30 22:49:11 +0530

    Reimplemented cart with useReducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer, useState } from 'react'
 import {
 	BrowserRouter as Router,
 	Routes,
@@ -12,10 +12,51 @@ import LoginPage from './pages/LoginPage'
 import ProductPage from './pages/ProductPage'
 import SearchPage from './pages/SearchPage'
 
+const save = state => {
+	window.localStorage.setItem('cart', JSON.stringify(state))
+	return state
+}
+
+const reducer = (state=[], action) => {
+	switch(action.type) {
+		case 'ADD_ITEM':
+			const item = action.payload
+			const _state = state
+			for (let i = 0; i < state.length; i++){
+				if(_state[i].id === item.id && _state[i].option === item.option){
+					_state[i].count++
+					return save(_state)
+				}
+			}
+			_state.push({...item, count: 1})
+			return _state
+		case 'REMOVE_ITEM':
+			const { id, option } = action.payload
+			const state_ = state
+			for (let i = 0; i<state_.length; i++){
+				if(state_[i].id === id && state_[i].option === option){
+					if(state_[i].count === 1){
+						return save([...state_.slice(0, i), ...state_.slice(i+1, state_.length)])
+					}else{
+						state_[i].count--
+						return save(state_)
+					}
+				}
+			}
+			return save(state)
+		case 'CLEAR':
+			return save([])
+		case 'RESTORE':
+			return save(action.payload)
+		default:
+			return save(state)
+	}
+}
+
 const App = () => {
 
 	const [user, setUser] = useState(null)
-	const [cart, setCart] = useState([])
+	const [cart, dispatch] = useReducer(reducer, [])
 
 	useEffect(() => {
 		const _user = JSON.parse(localStorage.getItem('user'))
@@ -24,42 +65,16 @@ const App = () => {
 			setUser(_user)
 		}
 		if(Array.isArray(_cart)) {
-			setCart(_cart)
+			dispatch({ type: 'RESTORE', payload: _cart })
 		}
 	}, [])
 
-	const addItem = item => {
-		const _cart = cart
-		const { id, option } = item
-		
-		for(let i=0; i<_cart.length; i++){
-			if (_cart[i].id === id && _cart[i].option === option){
-				_cart[i].count++
-				window.localStorage.setItem('cart', JSON.stringify(_cart))
-				setCart(_cart)
-				return
-			}
-		}
-		_cart.push({ ...item, count: 1 })
-		window.localStorage.setItem('cart', JSON.stringify(_cart))
-		setCart(_cart)
-	}
-
-	const removeItem = id => {
-		console.log(id)
-	}
-
-	const deleteItem = id => {
-		setCart(cart.filter(item => item.id!=id))
-	}
-
-	const clearCart = _ => {
-		setCart([])
-	}
+	useEffect(() => {
+	}, [cart])
 
 	return (
 		<AuthProvider value={{ user, setUser }} >
-			<CartProvider value={{cart, addItem, removeItem, deleteItem, clearCart}} >
+			<CartProvider value={{cart, dispatch}} >
 				<Router>
 					<Routes>
 						<Route path="/" element={<HomePage />} />
