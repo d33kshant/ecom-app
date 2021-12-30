41060363f7e784772de6483830e890214002101a
Deekshant Yadav <d33kshant@gmail.com>
2021-12-30 22:47:59 +0530

    Listed CartItems on CartPage

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,12 +1,24 @@
+import { useContext } from "react"
 import AppBar from "../components/AppBar"
+import CartItem from "../components/CartItem"
+import { CartContext } from "../contexts/CartContext"
 import '../styles/CartPage.css'
 
 const CartPage = () => {
+
+	const { cart } = useContext(CartContext)
+
 	return (
 		<>
 			<AppBar />
 			<div className=" cart-page-main">
 				<div className="cart-page-container">
+					<div className="cart-items-list" >
+						{ cart.map(item => <CartItem key={item.id} productId={item.id} count={item.count} option={item.option} />) }
+					</div>
+					<div className="cart-items-info" >
+						<p className="cart-info-title" >Your Cart</p>
+					</div>
 				</div>
 			</div>
 		</>
