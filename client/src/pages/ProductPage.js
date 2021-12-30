01af398d5032c300db71f1ca3f853ec7114eb058
Deekshant Yadav <d33kshant@gmail.com>
2021-12-30 22:48:32 +0530

    Updated onClick for add to cart

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -12,7 +12,7 @@ const ProductPage = () => {
 
 	const [currentImage, setCurrentImage] = useState(0)
 	const [currentOption, setCurrentOption] = useState(0)
-	const { addItem } = useContext(CartContext)
+	const { dispatch } = useContext(CartContext)
 	const [product, setProduct] = useState(null)
 
 	useEffect(() => {
@@ -45,7 +45,7 @@ const ProductPage = () => {
 						<div className="product-option-list" >
 							{ product.options.map((option, index)=> <button key={index} onClick={() => setCurrentOption(index)} className={index === currentOption ? "product-option-button selected-product-option" : "product-option-button"} >{option}</button> ) }
 						</div>
-						<button className="product-add-to-cart" onClick={()=>addItem({ id: product._id, option: product.options[currentOption] })} >Add To Cart</button>
+						<button className="product-add-to-cart" onClick={()=>{ console.log({ id: productID, option: product.options[currentOption] });dispatch({ type: 'ADD_ITEM', payload: { id: productID, option: product.options[currentOption] } })}} >Add To Cart</button>
 						<p className="product-info-description">{product.description}</p>
 					</div>
 				</div>
