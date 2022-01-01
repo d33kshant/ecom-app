053ac3ec6388000e58c53883590b255e9d406296
Deekshant Yadav <d33kshant@gmail.com>
2022-01-01 23:59:50 +0530

    Added Footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,9 @@
+import '../styles/Footer.css'
+
+const Footer = () => {
+	return (
+		<footer className="footer-main">©{(new Date()).getFullYear()} • made with &#129505; by<a href="https://link-list-psi.vercel.app/d33kshant" >@d33kshant</a></footer>
+	)
+}
+
+export default Footer
