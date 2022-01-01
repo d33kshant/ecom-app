7970fbcf92285b7575c1339db52b4efa8b5405a8
Deekshant Yadav <d33kshant@gmail.com>
2022-01-02 00:00:29 +0530

    Added onClick for Remove button

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -10,6 +10,7 @@ const CartItem = ({productId, count, option, callback}) => {
 
 	useEffect(() => {
 		axios.get(`/api/v1/product/${productId}`).then(res =>{ callback(res.data.rate*count, res.data.price*count);setProduct(res.data)})
+		return ()=>setProduct(null)
 	}, [productId])
 
 
@@ -35,7 +36,7 @@ const CartItem = ({productId, count, option, callback}) => {
 							<button className="cart-item-incr" onClick={()=>{dispatch({ type: 'ADD_ITEM', payload: { id: productId, option } })}} >+</button>
 							<p className="cart-item-count">{count}</p>
 							<button className="cart-item-decr" onClick={()=>{dispatch({type: 'REMOVE_ITEM', payload: { id: productId, option }})}} >-</button>
-							<button className="cart-item-remove">Remove</button>
+							<button className="cart-item-remove" onClick={()=>{dispatch({type: 'DELETE_ITEM', payload: { id: productId, option }})}} >Remove</button>
 						</div>
 					</div>
 				</>
