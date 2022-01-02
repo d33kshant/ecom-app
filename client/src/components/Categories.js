92b5d13dc1a6ea32d714a920da2ce0c718f61f4e
Deekshant Yadav <d33kshant@gmail.com>
2022-01-02 12:37:20 +0530

    Updated href for catrogries

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,11 +1,11 @@
 import '../styles/Categories.css'
 
 const Categories = () => {
-	const categories = [ 'All Products', 'Electronics', 'Mobiles', 'Fashion', 'Appliances', 'Toys', 'Books']
+	const categories = ['Electronics', 'Mobiles', 'Fashion', 'Appliances', 'Toys', 'Books']
 	return (
 		<div className="categories-main" >
 			<div className='categories-container' >
-				{ categories.map((category, index) => <a key={index} className="category-item" href={`/category/${category.replace(' ', '-').toLowerCase()}`} >{category}</a>) }
+				{ categories.map((category, index) => <a key={index} className="category-item" href={`/search?query=${category.replace(' ', '%20').toLowerCase()}`} >{category}</a>) }
 			</div>
 		</div>
 	)
