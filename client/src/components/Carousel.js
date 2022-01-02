7249ca73626668e6f2abb1592cc5c9a849922f42
Deekshant Yadav <d33kshant@gmail.com>
2022-01-02 12:27:24 +0530

    Added carousel image indicator

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -4,7 +4,6 @@ import '../styles/Carousel.css'
 const Carousel = () => {
 
 	const [index, setIndex] = useState(0)
-
 	
 	const images = [
 		"https://rukminim1.flixcart.com/flap/1688/280/image/407d75473a8dac3a.jpg?q=50",
@@ -38,6 +37,9 @@ const Carousel = () => {
 						<path fillRule="evenodd" d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"/>
 					</svg>
 				</button>
+				<div className="carousel-image-indicators" >
+					{ images.map((item, i)=><div onClick={()=>setIndex(i)} className={ i === index ? "carousel-current-indicator" : "carousel-image-indicator" } />) }
+				</div>
 			</div>
 		</div>
 	)
