32af0951214c1de307666f677f410a4076968317
Deekshant Yadav <d33kshant@gmail.com>
2022-01-03 14:17:59 +0530

    Added Feture to show error message

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -8,7 +8,7 @@ const LoginPage = () => {
 
 	const [state, setState] = useState({ email: "", password: "" })
 	const { user, setUser } = useContext(AuthContext)
-	const [ message, setMessage ] = useState("This is a default message")
+	const [ message, setMessage ] = useState('')
 
 	const onInputChange = (event) => {
 		const _state = { ...state }
@@ -21,7 +21,7 @@ const LoginPage = () => {
 		axios.post('/login', state).then(response => {
 			const { email, name, error } = response.data
 			if (error) {
-				console.log(error)
+				setMessage(error)
 			} else {
 				const data = { email, name }
 				setUser(data)
@@ -39,6 +39,7 @@ const LoginPage = () => {
 		<div className="login-page-main">
 			<div className="login-form-container">
 				<form className="login-form" onSubmit={onFormSubmit} >
+					<h2 className="login-form-title">Login</h2>
 					<input className="login-input-email" type="text" placeholder="Email" name="email" value={state.email} onChange={onInputChange} />
 					<input className="login-input-password" type="password" placeholder="Password" name="password" value={state.password} onChange={onInputChange} />
 					<button className="login-button-submit" type="submit">Login</button>
@@ -50,8 +51,8 @@ const LoginPage = () => {
 					message &&
 					<div className="login-message-box" >
 						<p className="login-message" >{message}</p>
-						<button className="login-message-close">
-							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+						<button className="login-message-close" onClick={()=>setMessage('')}>
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
 								<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
 							</svg>
 						</button>
