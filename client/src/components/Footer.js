d939070d810fcf54325435a4ef16e969d96ddcf5
Deekshant Yadav <d33kshant@gmail.com>
2022-01-04 00:35:10 +0530

    Updated Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import '../styles/Footer.css'
 
 const Footer = () => {
 	return (
-		<footer className="footer-main">©{(new Date()).getFullYear()} • made with &#129505; by<a href="https://link-list-psi.vercel.app/d33kshant" >@d33kshant</a></footer>
+		<footer className="footer-main">©{(new Date()).getFullYear()} • made with &#129505; by<a href="https://link-list-psi.vercel.app/d33kshant">@d33kshant</a></footer>
 	)
 }
 
