3b1cccb84f57f0c65a1f0f0667924f9da63a45ce
Deekshant Yadav <d33kshant@gmail.com>
2022-01-03 22:39:32 +0530

    Added route for signup page

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProductPage from './pages/ProductPage'
 import SearchPage from './pages/SearchPage'
+import SignUpPage from './pages/SignUpPage'
 
 const save = state => {
 	window.localStorage.setItem('cart', JSON.stringify(state))
@@ -83,6 +84,7 @@ const App = () => {
 								<Route path="/" element={<HomePage />} />
 								<Route path="/cart" element={<CartPage />} />
 								<Route path="/login" element={<LoginPage />} />
+								<Route path="/signup" element={<SignUpPage />} />
 								<Route path="/search" element={<SearchPage />} />
 								<Route path="/product/:id" element={<ProductPage />} />
 							</Routes>
