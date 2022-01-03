4618c6b4ce0a7ec4139b57a2be6652e1f97f23a0
Deekshant Yadav <d33kshant@gmail.com>
2022-01-03 22:35:12 +0530

    Removed callback

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -3,16 +3,15 @@ import { useContext, useEffect, useState } from 'react'
 import { CartContext } from '../contexts/CartContext'
 import "../styles/CartItem.css"
 
-const CartItem = ({productId, count, option, callback}) => {
+const CartItem = ({productId, count, option}) => {
 	
 	const [product, setProduct] = useState(null)
 	const { dispatch } = useContext(CartContext)
 
 	useEffect(() => {
-		axios.get(`/api/v1/product/${productId}`).then(res =>{ callback(res.data.rate*count, res.data.price*count);setProduct(res.data)})
+		axios.get(`/api/v1/product/${productId}`).then(res =>{setProduct(res.data)})
 		return ()=>setProduct(null)
 	}, [productId])
-
 
 	return (
 		<div className="cart-item-container" >
@@ -27,7 +26,7 @@ const CartItem = ({productId, count, option, callback}) => {
 							<span className="cart-item-mrp">{product.rate}₹</span>
 							{product.price !== product.rate && 
 							<>
-							• <span className="cart-item-offer">{Math.trunc((product.rate / product.price) * 100)}% off</span> •
+							• <span className="cart-item-offer">{Math.trunc(((product.price - product.rate) / product.price) * 100)}% off</span> •
 							<span className="cart-item-original">{product.price}₹</span>
 							</>}
 						</span>
@@ -35,7 +34,7 @@ const CartItem = ({productId, count, option, callback}) => {
 						<div className="cart-item-actions" >
 							<button className="cart-item-incr" onClick={()=>{dispatch({ type: 'ADD_ITEM', payload: { id: productId, option } })}} >+</button>
 							<p className="cart-item-count">{count}</p>
-							<button className="cart-item-decr" onClick={()=>{dispatch({type: 'REMOVE_ITEM', payload: { id: productId, option }})}} >-</button>
+							<button className="cart-item-decr" onClick={()=>{dispatch({type: 'REMOVE_ITEM', payload: { id: productId, option, price: product.rate, original: product.price }})}} >-</button>
 							<button className="cart-item-remove" onClick={()=>{dispatch({type: 'DELETE_ITEM', payload: { id: productId, option }})}} >Remove</button>
 						</div>
 					</div>
